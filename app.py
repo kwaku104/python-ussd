70f870c96c3de9b307dca4540941382fcb2f126f
Kwaku <kwakuappiahadu@gmail.com>
2019-12-05 16:41:24 +0000

    changed response again

--- app.py
+++ app.py
@@ -18,39 +18,40 @@ def ussd_callback():
     sms_phone_number = []
     sms_phone_number.append(phone_number)
 
-    #USSD Logic
+    #ussd logic
     if text == "":
         #main menu
         response = "CON What would you like to do?\n"
         response += "1. Check account details\n"
         response += "2. Check phone number\n"
-        response += "3. Send a cool message\n"
+        response += "3. Send me a cool message"
     elif text == "1":
         #sub menu 1
-        respone = "CON What would you like to check on your account?\n"
-        respone += "1. Account number"
+        response = "CON What would you like to check on your account?\n"
+        response += "1. Account number"
         response += "2. Account balance"
     elif text == "2":
-        # sub menu 1
+        #sub menu 1
         response = "END Your phone number is {}".format(phone_number)
     elif text == "3":
         try:
             #sending the sms
-            sms_response = sms.send("Thank you for trying out this app", sms_phone_number)
+            sms_response = sms.send("Thank you for going through this tutorial", sms_phone_number)
             print(sms_response)
         except Exception as e:
             #show us what went wrong
-            print(f"We have a problem: {e}")
+            print(f"Houston, we have a problem: {e}")
     elif text == "1*1":
-        #ussd menus are split *
+        #ussd menus are split using *
         account_number = "1243324376742"
-        response = "END Your account number is USD {}".format(account_number)
+        response = "END Your account number is {}".format(account_number)
     elif text == "1*2":
         account_balance = "100,000"
-        respone = "END Your account balance is USD {}".format(account_balance)
+        response = "END Your account balance is USD {}".format(account_balance)
     else:
-        respone = "END Invalid input. Try again."
-    return respone
+        response = "END Invalid input. Try again."
+
+    return response
 
 
 if __name__ == "__main__":
